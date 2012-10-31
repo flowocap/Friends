9efc4234458de9c4456e511beaef476da4fcf9c4
Robert Bruce Park <robert.park@canonical.com>
2012-10-31 12:30:47 +0100

    Add sender_id model schema column, with test fixups.
    This was requested by ronoc.

--- friends/tests/test_twitter.py
+++ friends/tests/test_twitter.py
@@ -121,14 +121,14 @@ oauth_signature="2MlC4DOqcAdCUmU647izPmxiL%2F0%3D"'''
         # This test data was ripped directly from Twitter's API docs.
         expected = [
             [[['twitter', 'faker/than fake', '240558470661799936']],
-             'messages', 'OAuth Dancer', 'oauth_dancer', False,
+             'messages', 'OAuth Dancer', '', 'oauth_dancer', False,
              '2012-08-28T21:16:23Z', 'just another test', '', '',
              'https://twitter.com/oauth_dancer/status/240558470661799936', '',
              '', '', '', 0.0, False, '', '', '', '', '', '', '', '', '', '',
              '', '', '', '', '', '', '', '', '', '',
              ],
             [[['twitter', 'faker/than fake', '240556426106372096']],
-             'messages', 'Raffi Krikorian', 'raffi', False,
+             'messages', 'Raffi Krikorian', '', 'raffi', False,
              '2012-08-28T21:08:15Z', 'lecturing at the "analyzing big data ' +
              'with twitter" class at @cal with @othman  http://t.co/bfj7zkDJ',
              '', '', 'https://twitter.com/raffi/status/240556426106372096', '',
@@ -136,7 +136,7 @@ oauth_signature="2MlC4DOqcAdCUmU647izPmxiL%2F0%3D"'''
              '', '', '', '', '', '', '', '', '', '',
              ],
             [[['twitter', 'faker/than fake', '240539141056638977']],
-             'messages', 'Taylor Singletary', 'episod', False,
+             'messages', 'Taylor Singletary', '', 'episod', False,
              '2012-08-28T19:59:34Z',
              'You\'d be right more often if you thought you were wrong.', '',
              '', 'https://twitter.com/episod/status/240539141056638977', '',
@@ -169,7 +169,7 @@ oauth_signature="2MlC4DOqcAdCUmU647izPmxiL%2F0%3D"'''
         # This test data was ripped directly from Twitter's API docs.
         expected_row = [
             [['twitter', 'faker/than fake', '240558470661799936']],
-            'messages', 'OAuth Dancer', 'oauth_dancer', True,
+            'messages', 'OAuth Dancer', '', 'oauth_dancer', True,
             '2012-08-28T21:16:23Z', 'just another test', '', '',
             'https://twitter.com/oauth_dancer/status/240558470661799936', '',
             '', '', '', 0.0, False, '', '', '', '', '', '', '', '', '', '',
