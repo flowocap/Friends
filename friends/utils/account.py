fe631ac18719437a3cdda3e0fdf3e4d7672f51f2
Robert Bruce Park <robert.park@canonical.com>
2013-03-18 22:52:19 -0700

    Simplify equality comparisons.

--- friends/utils/account.py
+++ friends/utils/account.py
@@ -165,11 +165,7 @@ class Account:
         return self.account_service.get_enabled()
 
     def __eq__(self, other):
-        if other is None:
-            return False
-        return self.account_service == other.account_service
+        return self.account_service == getattr(other, 'account_service', None)
 
     def __ne__(self, other):
-        if other is None:
-            return True
-        return self.account_service != other.account_service
+        return self.account_service != getattr(other, 'account_service', None)
