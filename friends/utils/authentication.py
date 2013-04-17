e199757dafa1fa8a664dd161ddab9ec500a69205
Robert Bruce Park <robert.park@canonical.com>
2013-04-17 10:35:05 -0700

    Do not raise unhandled auth exceptions from the MainThread. (LP: #1159552)
    Only raise them from the safety of subthreads, where they are caught and logged.

--- friends/utils/authentication.py
+++ friends/utils/authentication.py
@@ -41,6 +41,7 @@ class Authentication:
     def __init__(self, account):
         self.account = account
         self._reply = None
+        self._error = None
 
     def login(self):
         auth = self.account.auth
@@ -55,6 +56,13 @@ class Authentication:
             # callback gets called to give us the response to return.
             time.sleep(0.5)
             timeout -= 1
+        if self._error is not None:
+            exception = AuthorizationError(self.account.id, self._error.message)
+            # Mardy says this error can happen during normal operation.
+            if exception.message.endswith('userActionFinished error: 10'):
+                log.error(str(exception))
+            else:
+                raise exception
         if self._reply is None:
             raise AuthorizationError(self.account.id, 'Login timed out.')
         if 'AccessToken' not in self._reply:
@@ -64,12 +72,9 @@ class Authentication:
         return self._reply
 
     def _login_cb(self, session, reply, error, user_data):
-        self._reply = reply
+        # Don't raise Exceptions here because this callback runs in
+        # MainThread, not the thread you expect it to.
         if error:
-            exception = AuthorizationError(self.account.id, error.message)
-            # Mardy says this error can happen during normal operation.
-            if error.message.endswith('userActionFinished error: 10'):
-                log.error(str(exception))
-            else:
-                raise exception
-        log.debug('Login completed')
+            self._error = error
+        self._reply = reply
+        log.debug('_login_cb completed')
